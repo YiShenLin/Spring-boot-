9c24730512af3747f0cb0044dbbb882c6ef5776f
chtti <tony1102105341@gmail.com>
2020-06-18 16:46:29 +0800

    more case fot repository

--- src/main/java/com/chtti/springboot/demo/Demo7JPADocker/BeverageRunner1.java
+++ src/main/java/com/chtti/springboot/demo/Demo7JPADocker/BeverageRunner1.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,26 +15,66 @@ public class BeverageRunner1  implements CommandLineRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(BeverageRunner1.class.getSimpleName());
 
     @Autowired
-    MybeverageCRUDRepositry repositry;
+    MybeverageCRUDRepositry repository;
 
     @Override
     public void run(String... args) throws Exception {
         LOGGER.info("start to test beverage reposity");
         insertSomeData();
+        loadSomeData();
         loadAllData();
+        loadDataByPage();
+        loadDataByLike();
+        loadDataByContaining();
         deleteAndCheckCount();
     }
+
+    private void loadDataByContaining() {
+        LOGGER.info("find detail with milk");
+        repository.findByDetailContaining("milk")
+                .forEach(b -> LOGGER.info("find detail with milk:{}", b));
+        LOGGER.info("find detail with milk");
+        repository.findByDetailContaining("hot")
+                .forEach(b -> LOGGER.info("find detail with hot:{}", b));
+    }
+
+    private void loadDataByLike() {
+        LOGGER.info("find hot");
+        repository.findByTitleLike("hot%")
+                .forEach(beverage -> LOGGER.info("'hot prefix:'{}", beverage));
+        repository.findByTitleLike("%latte")
+                .forEach(beverage -> LOGGER.info("'end with latte:{}'", beverage));
+    }
+
+    private void loadDataByPage() {
+        LOGGER.info("load data by page ");
+        PageRequest pr1 =  PageRequest.of(0,4);
+        repository.findAll(pr1).forEach(beverage -> {LOGGER.info("#{} page:{}",pr1.getPageNumber(),beverage);});
+        Pageable pageable = pr1.next();
+        repository.findAll(pageable).forEach(beverage -> {LOGGER.info("#{} page:{}",pageable.getPageNumber(),beverage);});
+    }
+
+    private  void loadSomeData(){
+        LOGGER.info("find all americano only ");
+        repository.findMatchByTitle("americano").forEach(beverage -> LOGGER.info("get a record ==> {}",beverage));
+    }
     private  void loadAllData(){
-        repositry.findAll().forEach(beverage -> LOGGER.info("get a record ==> {}",beverage));
+        LOGGER.info("find all data");
+        repository.findAll().forEach(beverage -> LOGGER.info("get a record ==> {}",beverage));
     }
     private  void deleteAndCheckCount(){
-        LOGGER.info("before delete count={}",repositry.count());
-        repositry.deleteAll();
-        LOGGER.info("after delete count={}",repositry.count());
+        LOGGER.info("before delete count={}",repository.count());
+        repository.deleteAll();
+        LOGGER.info("after delete count={}",repository.count());
     }
     private void insertSomeData(){
-        repositry.save(new Beverage("americano","black coffee with no milk"));
-        repositry.save(new Beverage("latte","espresso with 60% milk"));
-        repositry.save(new Beverage("Assam black tea","normal black tea"));
+        repository.save(new Beverage("americano","ice black coffee with no milk"));
+        repository.save(new Beverage("americano","hot black coffee with no milk"));
+        repository.save(new Beverage("ice latte","espresso with 60% milk"));
+        repository.save(new Beverage("hot latte","espresso with 60% milk"));
+        repository.save(new Beverage("ice Assam black tea","normal black tea"));
+        repository.save(new Beverage("hot Assam black tea","normal black tea"));
+        repository.save(new Beverage("hot milk tea","black tea witch hot milk"));
+        repository.save(new Beverage("ice milk tea","black tea witch ice milk"));
    }
 }
--- src/main/java/com/chtti/springboot/demo/Demo7JPADocker/MybeverageCRUDRepositry.java
+++ src/main/java/com/chtti/springboot/demo/Demo7JPADocker/MybeverageCRUDRepositry.java
@@ -1,7 +1,15 @@
 package com.chtti.springboot.demo.Demo7JPADocker;
 
 import com.chtti.springboot.demo.Demo7JPADocker.bean.Beverage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MybeverageCRUDRepositry extends CrudRepository<Beverage,Long> {
+    List<Beverage> findMatchByTitle(String title);
+    Page<Beverage> findAll(Pageable pageable);
+    List<Beverage> findByTitleLike(String title);
+    List<Beverage> findByDetailContaining(String detail);
 }
